74bea1807edd00101e1727c9873888c8f6a53587
Sitthata <aprils96950@gmail.com>
2024-06-13 01:52:01 +0700

    implement form field using react hook form

--- src/components/FormGallery/AddNewForm.tsx
+++ src/components/FormGallery/AddNewForm.tsx
@@ -11,8 +11,26 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { IoMdDocument } from 'react-icons/io'
 import { Button } from '../ui/button'
+import { useForm } from 'react-hook-form'
+
+type TFormData = {
+  name: string
+  description: string
+}
 
 const AddNewForm = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TFormData>()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+    console.log(errors);
+  })
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -32,29 +50,28 @@ const AddNewForm = () => {
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Form name" />
+              <Input
+                id="name"
+                placeholder="Form name"
+                {...register('name', { required: true })}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name">Description</Label>
-              <Input id="name" placeholder="Description" />
+              <Input
+                id="name"
+                placeholder="Description"
+                {...register('description', { max: 150 })}
+              />
             </div>
           </div>
         </form>
         <DialogFooter>
-          <Button>Create Form</Button>
+          <Button onClick={onSubmit}>Create Form</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
   )
 }
-
-// const AddNewCard = () => {
-//   return (
-//     <div className="flex min-h-[10.725rem] cursor-pointer flex-col items-center justify-center gap-2 rounded-lg shadow-md outline outline-1 outline-accent">
-//       <IoMdDocument />
-//       Create New Form
-//     </div>
-//   )
-// }
 
 export default AddNewForm
