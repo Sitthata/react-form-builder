6a7076238cc966f1701b9980536dd26f18e39b20
Sitthata <aprils96950@gmail.com>
2024-06-21 00:30:27 +0700

    refactor: Update FormBlockStore to use createArrayManager utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,29 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function createArrayManager<T>(initialArray: T[] = []) {
+  let array = [...initialArray]
+
+  return {
+    getArray: () => array,
+    setArray: (newArray: T[]) => {
+      array = [...newArray]
+    },
+    push: (element: T) => {
+      array = [...array, element]
+    },
+    filter: (callback: (value: T, index: number, array: T[]) => boolean) => {
+      array = array.filter(callback)
+    },
+    update: (index: number, newElement: T) => {
+      array = [...array.slice(0, index), newElement, ...array.slice(index + 1)]
+    },
+    remove: (index: number) => {
+      array = [...array.slice(0, index), ...array.slice(index + 1)]
+    },
+    clear: () => {
+      array = []
+    },
+  }
+}
--- src/stores/FormBlockStore.ts
+++ src/stores/FormBlockStore.ts
@@ -1,3 +1,4 @@
+import { createArrayManager } from '@/lib/utils'
 import { create } from 'zustand'
 
 interface FormBlockState {
@@ -6,24 +7,42 @@ interface FormBlockState {
   updateBlock: (id: string, block: Block) => void
   removeBlock: (id: string) => void
 }
-const useFormBlockStore = create<FormBlockState>((set) => ({
-  // State
-  blocks: [],
+const useFormBlockStore = create<FormBlockState>((set) => {
+  const arrayManager = createArrayManager<Block>([])
+  function refresh() {
+    set({ blocks: arrayManager.getArray() })
+  }
+  return {
+    // State
+    blocks: arrayManager.getArray(),
 
-  // Actions
-  addBlock: (block) => set((state) => ({ blocks: [...state.blocks, block] })),
+    // Actions
+    addBlock: (block) => {
+      arrayManager.push(block)
+      refresh()
+    },
 
-  updateBlock: (id, updatedBlock) =>
-    set((state) => ({
-      blocks: state.blocks.map((block) =>
-        block.id === id ? { ...block, ...updatedBlock } : block
-      ),
-    })),
+    updateBlock: (id: string, updatedBlock: Partial<Block>) => {
+      const index = arrayManager
+        .getArray()
+        .findIndex((block) => block.id === id)
+      if (index !== -1) {
+        arrayManager.update(index, {
+          ...arrayManager.getArray()[index],
+          ...updatedBlock,
+        })
+        refresh()
+      }
+    },
 
-  removeBlock: (id) =>
-    set((state) => ({
-      blocks: state.blocks.filter((block) => block.id !== id),
-    })),
-}))
+    removeBlock: (id) => {
+      const index = arrayManager.getArray().findIndex((b) => b.id === id)
+      if (index !== -1) {
+        arrayManager.remove(index)
+        refresh()
+      }
+    },
+  }
+})
 
 export default useFormBlockStore
